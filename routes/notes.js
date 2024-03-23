c2bbd1be87cafbe52dc774e8bea3049b556de7a0
housejames <housejames14@gmail.com>
2024-03-22 17:22:10 -0700

    I have not been committing. I have been working on this for a while. Added changes to all js files

---
+++ routes/notes.js
@@ -0,0 +1,36 @@
+const fb = require('express').Router();
+const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the feedback
+fb.get('/', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+// POST Route for submitting feedback
+fb.post('/', (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text ) {
+    // Variable for the object we will save
+    const newNotes = {
+      title,
+      text,
+      text_id: crypto.randomUUID(),
+    };
+
+    readAndAppend(newNotes, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNotes,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting notes');
+  }
+});
+
+module.exports = fb;
